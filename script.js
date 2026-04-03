f7185f6402315e69f365db97a8a4b502cdc3e339
Azmat <147830024+Azmat2003@users.noreply.github.com>
2026-04-03 13:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let p = document.querySelectorAll(".price");
+
+	let sum = 0;
+	p.forEach((x) => {
+		sum += Number(p.textContent);
+	})
+	const tr = document.createElement("tr");
+	const td = document.createElement("td");
+
+	// Set colspan to span across both columns
+	td.setAttribute("colspan", "2");
+
+	td.textContent = "Total Price: " + total;
+
+	tr.appendChild(td);
+
+	// Append row to table
+	const table = document.querySelector("table");
+	table.appendChild(tr);
   
 };
 
