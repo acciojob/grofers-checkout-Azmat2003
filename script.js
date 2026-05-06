7c725bc6559c30e54bad3946bc159559e27234ff
Azmat <147830024+Azmat2003@users.noreply.github.com>
2026-05-06 18:10:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const prices = document.querySelectorAll(".prices");
+const btn = document.createElement("button");
+btn.textContent = "Get Total Price";
 
-let total = 0;
+document.body.appendChild(btn);
 
-prices.forEach((price) => {
-  total += Number(price.textContent);
+btn.addEventListener("click", function () {
+  const prices = document.querySelectorAll(".price");
+
+  let total = 0;
+
+  prices.forEach((price) => {
+    total += Number(price.textContent);
+  });
+
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+
+  td.setAttribute("colspan", "2");
+  td.textContent = `Total Price: ${total}`;
+
+  tr.appendChild(td);
+
+  document.querySelector("table").appendChild(tr);
 });
-
-const tr = document.createElement("tr");
-
-const td = document.createElement("td");
-
-td.setAttribute("colspan", "2");
-
-td.textContent = `Total Price: ${total}`;
-
-tr.appendChild(td);
-
-document.querySelector("table").appendChild(tr);
